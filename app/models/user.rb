6b5d956f82fa50c948b4bc3c840f39a96c6bcf19
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-08 13:38:39 +0900

    Add Signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # coding: utf-8
 class User < ApplicationRecord
   validates :name, format: { with: /\A[a-zA-Z]+\z/,
-                             message: "英数字のみが使えます"}
+                             message: "英数字のみが使えます"},
+            length: {maximum: 20}
   validates :profile, length: {maximum: 200}
   has_secure_password
 end
