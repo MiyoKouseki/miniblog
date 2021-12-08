fe2d88a760b2cba307da006b8861724977c0ce04
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-08 13:00:28 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_113220) do
+ActiveRecord::Schema.define(version: 2021_12_08_030739) do
 
   create_table "miniposts", force: :cascade do |t|
     t.text "content"
     t.datetime "datetime"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "profile"
+    t.text "blog_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
