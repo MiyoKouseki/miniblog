f7454cc0c7d9b2134481e533ef7de6b7957dbe15
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-08 13:19:15 +0900

    Allow User.name only has alphabet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+# coding: utf-8
 class User < ApplicationRecord
-  validates :profile, length {maximum: 200}
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+                             message: "英数字のみが使えます"}
+  validates :profile, length: {maximum: 200}
   has_secure_password
 end
