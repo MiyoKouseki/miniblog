b097baceb112faf9a62d7b302aac3da196754525
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-12 08:13:14 +0900

    update some validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_secure_password
+  validates :name, format: { with: /\A[a-zA-Z]+\Z/ }, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :profile, length: {maximum: 200}
 end
