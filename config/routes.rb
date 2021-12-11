cd6d6777849755f611af30aac8938f754338eda3
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-11 09:08:11 +0900

    generate Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/all'
   get 'users/new'
   root 'static_pages#home'
   
   get 'static_pages/home'
   get 'static_pages/help'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :miniposts
   resources :users
 end
