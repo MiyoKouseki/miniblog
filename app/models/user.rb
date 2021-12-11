d1c069648052083f277ad60b8f226774805e5139
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-12 08:42:17 +0900

    Add notification in user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+# coding: utf-8
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_secure_password
-  validates :name, format: { with: /\A[a-zA-Z]+\Z/ }, length: { maximum: 20 }
+  validates :name, format: {with: /\A[a-zA-Z]+\Z/,
+                            message: "alphabet is only available" },
+            length: { maximum: 20 }
+
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :profile, length: {maximum: 200}
 end
