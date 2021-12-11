0f1c4163c0fdd4e5e7762e497877e747c494f58a
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-11 11:25:45 +0900

    rails generate controller Microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 end
