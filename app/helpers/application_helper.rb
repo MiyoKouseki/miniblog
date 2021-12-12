f10c3c4b059810e5fdd95a0dc07d2d3e470e2ea2
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-12 18:00:40 +0900

    remove session controller because we use devise gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
-module ApplicationHelper
-
-  # def current_user?(user)
-  #   user && user == current_user
-  # end
-
-  # def store_location
-  #   use_session[:forwarding_url] = request.original_url if request.get?
-  # end
-
-  
+module ApplicationHelper  
 end
