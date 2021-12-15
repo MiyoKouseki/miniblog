c9ce7afca6c628ce50a7e57c41305089ea3d75cf
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-15 14:17:32 +0900

    rails generate model Relationship follower_id:integer followed_id:integer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 # coding: utf-8
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+class User < ApplicationRecord 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_many :microposts, dependent: :destroy
-  #has_secure_password
-  # validates :name, format: {with: /\A[a-zA-Z]+\Z/,
-  #                           message: "alphabet is only available" },
-  #           length: { maximum: 20 }
+  
 
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :email, presence: true, uniqueness: true,
+            format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :profile, length: {maximum: 200}
 
   def feed
