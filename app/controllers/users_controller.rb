9b40fcc69c3988948454d7603b7447c86ed56369
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-16 18:50:50 +0900

    DRY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,33 @@
 class UsersController < ApplicationController
   before_action :current_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+                                      :following, :followers]
+  before_action :set_user, only: [:show, :following, :followers]
+  
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])   
   end
 
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
   
   private
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
