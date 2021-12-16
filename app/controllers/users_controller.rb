9a2df14033c1247456d307b3cc851a721cc63068
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-16 19:21:27 +0900

    remove gomi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,5 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-  
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  
+    
 end
