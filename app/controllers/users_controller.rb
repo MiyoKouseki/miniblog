03d5ec1b3c978df563019f2f7f914e7617798fb8
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-16 18:03:07 +0900

    create, edit, new を消しても問題なかった。Deviseのおかげ？

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,26 +14,6 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.paginate(page: params[:page])   
   end
 
-  def new
-    @user = User.new
-  end
-
-  def edit
-    @user = User.find(params[:id])    
-  end
-  
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_back_or @user
-    else
-      render 'new'
-    end        
-  end
-
   def following
     @title = "Following"
     @user  = User.find(params[:id])
@@ -56,5 +36,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end        
+  end
+  
 end
