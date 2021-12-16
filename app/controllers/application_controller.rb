fddaff8f7f6baded63fe2358d79d330c0c517dd1
MiyoKouseki <kouseki.miyo@gmail.com>
2021-12-16 12:47:18 +0900

    Devise の認証につかうパラメータに name を追加したらできた。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?  
   before_action :authenticate_user!, only: [:index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+    def configure_permitted_parameters
+      added_attrs = [:name, :email,
+                     :password,:password_confirmation]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end  
+  
   private  
     def production?
       Rails.env.production?
